8dfeaa270398024e788bee41db51fa1ec99ba973
1303721 <mrdrift@hotmail.co.uk>
2015-10-19 15:39:23 +0100

    added a paragraph to get this to work

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+    <body>
+<p>
+    <?php
+    echo "hello world";
+    ?>
+</p>
+</body>
+
+</html>
+
+
+/**
+ * Created by PhpStorm.
+ * User: 1303721
+ * Date: 19/10/2015
+ * Time: 15:36
+ */
