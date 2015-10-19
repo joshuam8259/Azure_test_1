fc3db5bd3964aeeda81117a120f19146207b2ae5
1303721 <mrdrift@hotmail.co.uk>
2015-10-19 16:24:09 +0100

    added a paragraph to get this to work

--- index.php
+++ index.php
@@ -6,15 +6,24 @@
 <p>
     <?php
     echo "test test test test test ";
-$name = "Edgar";
-
-    if($name == "simon"){
-        print "I know you!";
-    }
-    else {
-        print "Who are you?";
-    }
     echo "the user namme is " . $name ;
+$name = "Edgar";
+    $myage = 18;
+
+    if($myage < 16){
+        print "You are to young";
+    }
+    elseif ($myage >= 21){
+        print "you can buy specs,mugs and sasuage rolls ";
+    }
+    elseif ($myage <= 21 ) {
+        print "you can buy mugs and specs";
+    }
+    elseif ($myage < 18){
+        print "you can buy specs";
+    }
+
+
 
     ?>
 </p>
