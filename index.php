3bd85f8426fbcb040a7a9ca36ef4172d96ecdfc6
1303721 <mrdrift@hotmail.co.uk>
2015-10-19 15:52:05 +0100

    added a paragraph to get this to work

--- index.php
+++ index.php
@@ -5,12 +5,9 @@
     <body>
 <p>
     <?php
-    echo "Hello World";
-   echo "wil this work as well ";
-
     $myname = "frodo baggins";
     $myage = 111;
-    echo "my name is" . $myname . "and i am" . $myage;
+    echo "my name is " . $myname . " and i am" . $myage;
     ?>
 </p>
 </body>
