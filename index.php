78a1f0d7b4d310c78e89049709d5dfd7ca355176
1303721 <mrdrift@hotmail.co.uk>
2015-10-19 15:53:07 +0100

    added a paragraph to get this to work

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
     <?php
     $myname = "frodo baggins";
     $myage = 111;
-    echo "my name is " . $myname . " and i am" . $myage;
+    echo "my name is " . $myname . " and i am " . $myage;
+    //this should not been seen
+    /* or this either */
     ?>
 </p>
 </body>
