d6741dcb83091ba3bde116d05df582bb2fcf2e9c
1303721 <mrdrift@hotmail.co.uk>
2015-10-19 15:59:02 +0100

    added a paragraph to get this to work

--- index.php
+++ index.php
@@ -5,11 +5,15 @@
     <body>
 <p>
     <?php
-    $myname = "frodo baggins";
-    $myage = 111;
-    echo "my name is " . $myname . " and i am " . $myage;
-    //this should not been seen
-    /* or this either */
+    echo "test test test test";
+$name = "Edgar";
+
+    if($name == "simon"){
+        print "I know you!";
+    }
+    else {
+        print "who are you?";
+    }
     ?>
 </p>
 </body>
