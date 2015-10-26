7b7745282464168120cb1efcd0b505290c7727a5
1303721 <mrdrift@hotmail.co.uk>
2015-10-26 16:06:01 +0000

    added a paragraph to get this to work

--- index.php
+++ index.php
@@ -32,6 +32,9 @@
 
 
     ?>
+<p><a href="http://jm1234.azurewebsites.net/helloprinter.php?query=Earth">Earth</a></p>
+<p><a href="http://jm1234.azurewebsites.net/helloprinter.php?query=Mars"></a>Mars</p>
+<p><a href="http://jm1234.azurewebsites.net/helloprinter.php?query=Uranus"></a>Uranus</p>
 </p>
 </body>
 
